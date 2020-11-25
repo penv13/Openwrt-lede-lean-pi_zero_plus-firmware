efe818ec15318af490e62dc3a4f3838faf90d002
penv13 <60926544+penv13@users.noreply.github.com>
2020-11-25 14:26:10 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -6,10 +6,10 @@
 # Blog: https://p3terx.com
 #=================================================
 rm -rf /package/luci-theme-argon
-git clone https://github.com/jerrykuku/luci-theme-argon.git /package/luci-theme-argon
-git clone https://github.com/jerrykuku/lua-maxminddb.git /package/lua-maxminddb
-git clone https://github.com/jerrykuku/luci-app-vssr.git /package/luci-app-vssr
-git clone https://github.com/vernesong/OpenClash.git /package/luci-app-openclash
+git clone https://github.com/jerrykuku/luci-theme-argon.git /package/lean/luci-theme-argon
+git clone https://github.com/jerrykuku/lua-maxminddb.git /package/lean/lua-maxminddb
+git clone https://github.com/jerrykuku/luci-app-vssr.git /package/lean/luci-app-vssr
+git clone https://github.com/vernesong/OpenClash.git /package/lean/luci-app-openclash
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.184/g' package/base-files/files/bin/config_generate
